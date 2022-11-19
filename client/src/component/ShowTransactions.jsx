cdf8816fce0253e2fadd36f0f0c385e5483ca994
farisamirmudin <farisamirmudin@gmail.com>
2022-11-19 18:27:28 +0800

    change show transaction

--- client/src/component/ShowTransactions.jsx
+++ client/src/component/ShowTransactions.jsx
@@ -7,18 +7,16 @@ const ShowTransactions = ()=> {
   const [ishidden, setIsHidden] = useState(true)
   if (transactions.length) {
     return (
-      <div className='pt-10 pb-20 text-center '>
-        <p className='text-4xl'>Transactions</p>
-        <p className='text-sm mb-12 text-gray-600'>Click on the date for more info</p>
-        <div className="flex md:flex-row flex-col gap-5 flex-wrap box-content justify-center">
+      <div className='pt-10 pb-20 self-center'>
+        <p className='text-4xl text-center'>Transactions</p>
+        <p className='text-sm mb-12 text-gray-600 text-center'>Click on the date for more info</p>
+        <div className="">
           {transactions.map((transaction, i) => (
-            <div className='bg-slate-200 p-4 rounded-xl' key={i}>
+            <div className='bg-slate-200 px-4 py-2 my-3 rounded-xl' key={i}>
               <div className="hover:cursor-pointer" onClick={() => setIsHidden(prev => !prev)}>{transaction.timestamp}</div>
               <div className={`${ishidden ? "hidden" : ""}`}>
-                <div className="border-t-2 border-slate-300 my-4"></div>
-                <p><strong>From:</strong> {addressLink(transaction.sender)}</p>
                 <p><strong>To:</strong> {addressLink(transaction.receiver)}</p>
-                <div className="border-t-2 border-slate-300 my-4"></div>
+                <p><strong>From:</strong> {addressLink(transaction.sender)}</p>
                 <p><strong>Amount:</strong> {transaction.amount} ETH</p>
                 <p><strong>Message:</strong> {transaction.message}</p>
               </div>
