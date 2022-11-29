9462ed9575de8e6a2ab52eaefbbfad505bef4dcc
farisamirmudin <farisamirmudin@gmail.com>
2022-11-29 23:35:13 +0800

    changed cryptchange to kryptransfer

--- client/src/component/Body.jsx
+++ client/src/component/Body.jsx
@@ -8,7 +8,7 @@ const Body = () => {
       <div className="flex gap-6 py-8 md:flex-row flex-col justify-between">
         <div className="space-y-6">
           <p className='lg:text-6xl text-5xl'>Send crypto across the internet</p>
-          <p className='text-gray-600'>Explore Web 3.0 and Blockchain technology. Buy and sell crypto here on Cryptchange</p>
+          <p className='text-gray-600'>Explore Web 3.0 and Blockchain technology. Buy and sell crypto here on Kryptransfer</p>
           <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
             {['Reliability', 'Security', 'Blockchain', 'Web 3.0', 'Metamask', 'Ethereum'].map((item, i) =>
               <div key={i} className='bg-slate-200 py-2 px-4 text-center shadow-md rounded-lg'>
