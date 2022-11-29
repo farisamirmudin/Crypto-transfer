f241386fb60135800d5c1ed92b9b9d2af0d51d2d
farisamirmudin <farisamirmudin@gmail.com>
2022-11-29 22:57:51 +0800

    changed connect wallet to connect

--- client/src/component/ConnectButton.jsx
+++ client/src/component/ConnectButton.jsx
@@ -4,7 +4,7 @@ import { TransactionContext } from "../context/TransactionContext"
 const ConnectButton = () => {
   const { connectWallet, hasMetamask, currentAccount } = useContext(TransactionContext)
   return (
-    <button onClick={connectWallet} disabled={!hasMetamask} className={`px-4 py-2 bg-blue-600 hover:bg-blue-500 rounded-lg text-white`}>{currentAccount ? 'Connected' : 'Connect Wallet'}</button>
+    <button onClick={connectWallet} disabled={!hasMetamask} className={`px-4 py-2 bg-blue-600 hover:bg-blue-500 rounded-lg text-white`}>{currentAccount ? 'Connected' : 'Connect'}</button>
   )
 }
 
